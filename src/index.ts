cfbf2e132e832afec6b9bc51806331dd6af11f8d
Augusto Custodio <custodioovicente@outlook.com.br>
2023-07-30 20:09:39 -0300

    import external libraries (validator example)

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,7 @@
-//const Matematica = require("./Matematica");
-//import * as Matematica from "./Matematica";
-//import {somar, subtrair} from './Matematica'
-import Matematica from "./Matematica";
+import validator from 'validator';
 
-let n1: number = 10;
-let n2: number = 2;
+let ip = '1.1.1.1000'
 
-console.log(`Soma: ${Matematica.somar(n1, n2)}`);
-console.log(`Subtração: ${Matematica.multiplicar(n1, n2)}`);
-console.log(`Multiplicação: ${Matematica.subtrair(n1, n2)}`);
+console.log(validator.isIP(ip));
+
+console.log(validator.isEmail('suporteb7web.com'));
