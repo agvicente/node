6b0323f30d1095cdcd0bbe18a94b2de95aafcb53
Augusto Custodio <custodioovicente@outlook.com.br>
2023-07-30 22:11:20 -0300

    routes (statics and dinamics)

--- src/server.ts
+++ src/server.ts
@@ -6,4 +6,9 @@ server.get('/', (req: Request, res: Response) => {
     res.send('<h1>Hello World!</h1>');
 });
 
+server.get('/noticias/:slug', (req: Request, res: Response) => {
+    let slug: string = req.params.slug;
+    res.send(`Noticia ${slug}`);
+});
+
 server.listen(3000);
